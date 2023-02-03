560e90e65f6fdda9598b59219251b08c9f50584e
pmeinhardt <mnhrdt@gmail.com>
2023-02-03 11:31:42 +0100

    Allow ranking module to be used with custom params
    While we have to hard-code parameters for using the module via Redis
    functions, we can now use its value encoding functionality with custom
    parameters from other Lua code. This enables, for instance, testing of
    the code with different configurations.

--- modules/ranking.lua
+++ modules/ranking.lua
@@ -5,6 +5,8 @@
 -- Members with equal scores are ordered by the time of when the score was last
 -- updated, with members who achieved the score earlier being listed first.
 --
+-- Scores are assumed to be integer values.
+--
 -- Functions
 --
 --   FCALL rkincrby 1 key increment member
@@ -65,7 +67,7 @@
 --   maximum = minimum +
 --             math.floor(2^(54 - params.time.nbits) / 10^params.score.scale)
 --
---   for the default parameters (with scale=0, integer score values):
+--   for the default parameters (min=0):
 --
 --   minimum = 0
 --   maximum = 4194304
@@ -88,8 +90,11 @@
 --   time resolution by adjusting params.score.min. If this range is too narrow
 --   for you, you may choose to increase the minimum value, sacrificing time
 --   resolution for those entries at the bottom of the ranking below the
---   minimum value, but gaining room at the top end. Or you may set a negative
---   scale if you know your scores are always mutliples of 10, 100, ….
+--   minimum value, but gaining room at the top end.
+--
+--   If you know your scores are always mutliples of 10, 100, … or some other
+--   factor, you can divide them by this factor before passing them in and
+--   multiply any retrieved values to get back your original score.
 --
 
 local errors = {
@@ -97,31 +102,18 @@ local errors = {
   nkeys = "ERR wrong number of keys",
 }
 
-local params = {
+local defaults = {
   time = {
     nbits = 32, -- number of bits to use for time information
     scale = 0, -- decimal digits in the fractional part, 0=1s, 1=0.1s, 2=0.01s…
     min = 1672531200, -- min. expected unix time: 2023-01-01 00:00:00 UTC
   },
   score = {
-    scale = 0, -- decimal digits in the fractional part of score values
     min = 0, -- min. expected score
   },
 }
 
-local tbits = params.time.nbits
-local step = 2^tbits
-
 local maxsafe = 2^53
-local anchor = step / 2 - maxsafe
-
-local tinc = 10^(6 - params.time.scale)
-local tmin = params.time.min * 1000000
-local tmax = tmin + (2^tbits - 1) * tinc
-
-local sinc = 10^params.time.scale
-local smin = params.score.min
-local smax = smin + 2^(53 + 1 - tbits) / 10^params.score.scale - 2
 
 local function round (value)
   return math.floor(value + 0.5)
@@ -131,19 +123,47 @@ local function clamp (value, min, max)
   return math.min(math.max(value, min), max)
 end
 
-local function encode (score, timestamp)
-  local left = round((score - smin) / sinc) * step
-  local time = clamp(round((timestamp - tmin) / tinc), 0, step - 1)
-  local right = step / 2 - 1 - time
-  return anchor + left + right
+local function init (params)
+  local o = {}
+  local tbits = params.time.nbits
+
+  o.tbits = tbits
+  o.step = 2^tbits
+
+  o.anchor = o.step / 2 - maxsafe
+
+  o.tinc = 10^(6 - params.time.scale)
+  o.tmin = params.time.min * 1000000
+  o.tmax = o.tmin + (2^tbits - 1) * o.tinc
+
+  o.smin = params.score.min
+  o.smax = o.smin + 2^(53 + 1 - tbits) - 2
+
+  return o
 end
 
-local function decode (value)
+local Codec = {}
+
+function Codec:new (params)
+  local codec = init(params or defaults)
+  setmetatable(codec, self)
+  self.__index = self
+  return codec
+end
+
+function Codec:encode (score, timestamp)
+  local left = round((score - self.smin)) * self.step
+  local time = clamp(round((timestamp - self.tmin) / self.tinc), 0, self.step - 1)
+  local right = self.step / 2 - 1 - time
+  return self.anchor + left + right
+end
+
+function Codec:decode (value)
   -- We never use the stored timestamp, so we don't bother extracting it
-  return (round((value - step / 2) / step) + 2^(53 - tbits)) * sinc + smin
+  return (round((value - self.step / 2) / self.step) + 2^(53 - self.tbits)) + self.smin
 end
 
-local function incrby (keys, args)
+local function incrby (codec, keys, args)
   if #keys ~= 1 then return redis.error_reply(errors.nkeys) end
   if #args ~= 2 then return redis.error_reply(errors.nargs) end
 
@@ -154,7 +174,7 @@ local function incrby (keys, args)
   local score
 
   if value then
-    score = decode(value)
+    score = codec:decode(value)
   else
     score = 0
   end
@@ -164,28 +184,22 @@ local function incrby (keys, args)
   local seconds, microseconds = unpack(redis.call("TIME"))
   local timestamp = seconds * 1000000 + microseconds
 
-  value = encode(score, timestamp)
+  value = codec:encode(score, timestamp)
 
   redis.call("ZADD", key, value, member)
 
   return score
 end
 
-local function score (keys, args)
+local function score (codec, keys, args)
   if #keys ~= 1 then return redis.error_reply(errors.nkeys) end
   if #args ~= 1 then return redis.error_reply(errors.nargs) end
   local value = redis.call("ZSCORE", keys[1], args[1])
   if value == nil then return nil end
-  return decode(value)
+  return codec:decode(value)
 end
 
-local function rank (keys, args)
-  if #keys ~= 1 then return redis.error_reply(errors.nkeys) end
-  if #args ~= 1 then return redis.error_reply(errors.nargs) end
-  return redis.call("ZREVRANK", keys[1], args[1])
-end
-
-local function range (keys, args)
+local function range (codec, keys, args)
   if #keys ~= 1 then return redis.error_reply(errors.nkeys) end
   if #args ~= 2 then return redis.error_reply(errors.nargs) end
 
@@ -195,29 +209,35 @@ local function range (keys, args)
   local res = {}
 
   for i=1,#rng,2 do
-    res[i], res[i + 1] = rng[i], decode(rng[i + 1])
+    res[i], res[i + 1] = rng[i], codec:decode(rng[i + 1])
   end
 
   return res
 end
 
+local function rank (keys, args)
+  if #keys ~= 1 then return redis.error_reply(errors.nkeys) end
+  if #args ~= 1 then return redis.error_reply(errors.nargs) end
+  return redis.call("ZREVRANK", keys[1], args[1])
+end
+
 if redis then
   local prefix = "rk"
 
-  redis.register_function(prefix .. "incrby", incrby)
-  redis.register_function(prefix .. "score", score)
-  redis.register_function(prefix .. "rank", rank)
-  redis.register_function(prefix .. "range", range)
+  local function rkincrby (...) return incrby(Codec:new(defaults), ...) end
+  local function rkscore (...) return score(Codec:new(defaults), ...) end
+  local function rkrange (...) return range(Codec:new(defaults), ...) end
+  local rkrank = rank -- no codec needed
+
+  redis.register_function(prefix .. "incrby", rkincrby)
+  redis.register_function(prefix .. "score", rkscore)
+  redis.register_function(prefix .. "range", rkrange)
+  redis.register_function(prefix .. "rank", rkrank)
 end
 
 local exports = {
-  decode = decode,
-  encode = encode,
-  tmin = tmin,
-  tmax = tmax,
-  tinc = tinc,
-  smin = smin,
-  smax = smax,
+  new = function (...) return Codec:new(...) end,
+  defaults = defaults,
 }
 
 return exports
